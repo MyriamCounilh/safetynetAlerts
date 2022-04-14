645feafd844a56ba5110083498b4a8658256aedc
MyriamCounilh <myriam.counilh@aticdnet.com>
2022-04-14 12:20:49 +0200

    Creation du gitFlow

--- src/main/java/com/myriamcounilh/safetynetalerts/SafetynetAlertsApplication.java
+++ src/main/java/com/myriamcounilh/safetynetalerts/SafetynetAlertsApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SafetynetAlertsApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(SafetynetAlertsApplication.class, args);
     }
+
 
 }

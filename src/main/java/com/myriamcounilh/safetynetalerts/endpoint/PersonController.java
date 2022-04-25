98f3564f82a108a5f43b49afb7aea66055bb6ddb
MyriamCounilh <myriam.counilh@aticdnet.com>
2022-04-25 18:11:44 +0200

    Revue de code

--- src/main/java/com/myriamcounilh/safetynetalerts/endpoint/PersonController.java
+++ src/main/java/com/myriamcounilh/safetynetalerts/endpoint/PersonController.java
@@ -13,17 +13,6 @@ import org.springframework.web.server.ResponseStatusException;
 import javax.validation.Valid;
 import java.util.List;
 
-
-/**
- * TODO : http://localhost:8080/person
- * Cet endpoint permettra d’effectuer les actions suivantes via Post/Put/Delete avec HTTP :
- * ● ajouter une nouvelle personne ;
- * ● mettre à jour une personne existante (pour le moment, supposons que le prénom et le nom de
- * famille ne changent pas, mais que les autres champs peuvent être modifiés) ;
- * ● supprimer une personne (utilisez une combinaison de prénom et de nom comme identificateur
- * unique).
- */
-
 @RestController
 @RequestMapping("/person")
 public class PersonController {
